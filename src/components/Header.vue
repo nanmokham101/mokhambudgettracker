e81a9eba4a0e1344e61a9c46b2946f37e52b8bd4
innoveller <mokham29580@gmail.com>
2022-08-03 10:49:32 +0630

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <h1>Budget Tracker</h1>
+    <div class="total-income">{{ totalIncome }} MMK</div>
+  </header>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+export default {
+  props: {
+    totalIncome: Number
+  },
+  components:{Header}
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+  background-color: #7c409A;
+  border-bottom: 5px solid #000;
+}
+header h1 {
+  color: #EEE;
+  font-size: 28px;
+}
+header .total-income {
+  font-family: 'Fira Code', 'Fira Sans', sans-serif;
+  background-color: #000;
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 900;
+  padding: 5px 10px;
+  min-width: 100px;
+  text-align: center;
+  border-radius: 8px;
+  box-shadow: inset 0px 0px 6px rgba(0, 0, 0, 0.25);
+  text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.25);
+}
+</style>
---
+++ src/components/IncomeList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="income-list">
+    <IncomeItem v-for="obj in state.sortedIncome" :key="obj.id" :income="obj" @remove-item="removeItem" />
+  </div>
+</template>
+
+<script>
+import IncomeItem from './IncomeItem';
+export default {
+  components:{IncomeItem},
+  props: {
+    state: Object
+  },
+  setup (props, { emit }) {
+    function removeItem(id) {
+      emit( 'remove-item', id );
+    }
+    return {
+      removeItem
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .income-list {
+    margin-top: 30px;
+    padding: 15px;
+  }
+</style>
